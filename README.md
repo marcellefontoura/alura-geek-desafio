897d9176595494133a9afb1cfc76df15bd0a672a
Marcelle Fontoura <marcelle_sfs@hotmail.com>
2024-07-16 19:19:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Challenge ONE | Front End - Alura-Geek
+---
+
+<p align="center" >
+     <img width="600" heigth="600" src="assets/projeto.png">
+</p>
+
+---
+### Este é o repositório base do projeto, nele voce encontrará:
+#### 🔹index.html: Documento HTML finalizado que você pode utilizar em seu projeto. Fonte, CSS e JavaScript já linkado e todas as sessões comentadas; 
+#### 🔹style.css: Documento de CSS com instruções de estilo e também algumas sugestões para o desenvolvimento;
+#### 🔹validacao.js: Documento vazio para que possa aplicar sua lógica de programação, trabalhando com a validação de formulário em JavaScript.
---
+++ js/conectaApi.js
@@ -0,0 +1,40 @@
+async function listaProdutos() {
+    const conexao = await fetch("http://localhost:3000/produtos");
+    const conexaoConvertida = await conexao.json();
+    
+    return conexaoConvertida;
+}
+
+async function criaProduto(nome, imagem, preco) {
+    const conexao = await fetch("http://localhost:3000/produtos", {
+        method: "POST", 
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            imagem: imagem,
+            preco: preco,
+        })
+    });
+    
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function removeProduto(itensProduto) {
+    const conexao = await fetch("http://localhost:3000/produtos/deletar?itens=" + itensProduto, {
+        method: "DELETE"
+    });
+    
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+export const conectaApi = {
+    listaProdutos,
+    criaProduto,
+    removeProduto
+}
